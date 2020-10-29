0ce1212f9984caf0eaf4509b2e6dccdebe3a48cd
chrissuda <ab792638826@gmailcom>
2020-10-29 18:56:48 -0400

    Filter

--- depth_map/Filter.py
+++ depth_map/Filter.py
@@ -12,24 +12,15 @@ def toNumpy(x):
         
     return x
 
-def filterDoor(bounding_boxes,label,confidence_score,new_size):
+def filterDoor(bounding_boxes,label,confidence_score,new_size,image_id,folder):
     """
     @param list  Object candidate bounding boxes 
     @param label predicted category
     @param list  Confidence score of bounding boxes
-    @param pointCloud
     @return list bounding_boxes,label,confidence_score
     """
     
-
-    #Mapping from cropped_image to panorama
-    pano_x,pano_y=16384,8192
-    pointCloud_x,pointCloud_y=512,256
-    img_width,img_height=3584.0,2560.0
-    width_left_offset=2048
-    width_right_offset=10752
-    height_offset=3072
-    distance_threshold=0.25
+    z_threshold=0.25
 
     # Bounding boxes
     bounding_boxes = toNumpy(bounding_boxes)
@@ -52,25 +43,13 @@ def filterDoor(bounding_boxes,label,confidence_score,new_size):
     x_right=bounding_boxes[door_index[0],2]
     y_bottom=bounding_boxes[door_index[0],3]
 
-    #Create a y_threshold to determine what is the height different in y pixels
-    #between a true door and doors that need to be verified 
-    img_y=y_bottom*(img_height/new_size[1])+height_offset
-    img_y=int(img_y/pano_y*pointCloud_y)
-    img_x=x_right*(img_height/new_size[1])+width_left_offset
-    img_x=int(img_x/pano_x*pointCloud_y)
-    z0=pointCloud[img_y*512+img_x,3]
-    z=0
-    y_threshold=0
-    while z<distance_threshold:
-        y_threshold+=1
-        z=pointCloud[(img_y-y_threshold)*512+img_x,3]-z0
 
-    y_threshold=y_threshold/pointCloud_y*new_size[1]
 
     #Make a threshold here and take out some doors to verify
-    door_filter_index=np.where(bounding_boxes[door_index,3]<y_bottom-y_threshold)
+    z0=getZ(x_right,y_bottom,new_size,image_id,folder)
+    z=getZ(bounding_boxes[door_index[i],2],bounding_boxes[door_index[i],3],new_size,image_id,folder)
+    door_filter_index=np.where(z-z0>z_threshold)
     door_filter_index=door_filter_index[0]
-    
 
     x1=bounding_boxes[door_index[door_filter_index],0]
     y1=bounding_boxes[door_index[door_filter_index],3]
@@ -88,14 +67,18 @@ def filterDoor(bounding_boxes,label,confidence_score,new_size):
     sr_x1=sr_x1.T
     boolean_x=np.logical_and(x1-(x2-x1)*1/2<sr_x1,sr_x1<x1+(x2-x1)*1/2)
 
+
     #y1-(y2-y1)*1/2<sr_y1<y1+(y2-y1)*1/2
-    boolean_y=np.ones((y1.size,sr_y1.size))
-    sr_y1=boolean_y*sr_y1
-    sr_y1=sr_y1.T
-    boolean_y=np.logical_and(y1-(y2-y1)*1/2<sr_y1,sr_y1<y1+(y2-y1)*1/2)
+    z1=getZ(x1,y1,new_size,image_id,folder)
+    sr_z1=getZ(sr_x1,sr_y1,new_size,image_id,folder)
+    boolean_z=np.ones((z1.size,sr_z1.size))
+    sr_z1=boolean_z*sr_z1
+    sr_z1=sr_z1.T
+
+    boolean_z=np.logical_and(z1-z_threshold<sr_z1,sr_y1<z1+z_threshold)
     
     #boolean and between (boolean of x and y)
-    boolean_door=np.logical_and(boolean_x,boolean_y)
+    boolean_door=np.logical_and(boolean_x,boolean_z)
 
     #boolean or in each row
     boolean_door=np.sum(boolean_door,axis=1)
@@ -109,8 +92,30 @@ def filterDoor(bounding_boxes,label,confidence_score,new_size):
     return bounding_boxes,label,confidence_score
 
 
-bounding_boxes=[[50,50,70,70],[10,40,30,60],[10,53,25,70],[80,40,100,60],[85,62,100,80]]
-label=[1,1,4,1,3]
-confidence_score=[0.8,0.4,0.4,0.3,0.3]
-boxes,label,score=filterDoor(bounding_boxes,label,confidence_score)
-print(boxes,"\n\n",label,"\n\n",score)
+# bounding_boxes=[[50,50,70,70],[10,40,30,60],[10,53,25,70],[80,40,100,60],[85,62,100,80]]
+# label=[1,1,4,1,3]
+# confidence_score=[0.8,0.4,0.4,0.3,0.3]
+# boxes,label,score=filterDoor(bounding_boxes,label,confidence_score)
+# print(boxes,"\n\n",label,"\n\n",score)
+
+def getZ(x,y,new_size,image_id,folder):
+    #Mapping from cropped_image to panorama
+    pano_x,pano_y=16384,8192
+    pointCloud_x,pointCloud_y=512,256
+    img_width,img_height=3584.0,2560.0
+    width_left_offset=2048
+    width_right_offset=10752
+    height_offset=3072
+
+    y=int((y/new_size[1]*img_height+height_offset)*pointCloud_y/pano_y)
+    if image_id[-5]=="0":
+        x=int((x/new_size[0]*img_width+width_left_offset)*pointCloud_x/pano_x)
+
+    elif image_id[-5]=="1":
+        x=int((x/new_size[0]*img_width+width_right_offset)*pointCloud_x/pano_x)
+
+    path=os.path.join(folder,image_id[:-6]+".npy")
+    pointCloud=np.load(path)
+
+    return pointCloud[y*img_width+x,2]
+
